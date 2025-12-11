05ec1a302a4dccb11886a04482c71003958d5239
Hemanth1234554 <hemanthkummari5@gmail.com>
2025-12-11 23:18:16 +0530

    Rollbacked

--- server/routes/alertRoutes.js
+++ server/routes/alertRoutes.js
@@ -1,14 +1,9 @@
 // File: server/routes/alertRoutes.js
 import express from 'express';
-import { createAlert, getAlerts } from '../controllers/alertController.js'; 
-import { protect } from '../middleware/authMiddleware.js';
+import { createAlert } from '../controllers/alertController.js';
 
 const router = express.Router();
 
-// Route to SEND an alert (SOS)
-router.post('/', protect, createAlert);
-
-// Route to GET history (The Vault)
-router.get('/', protect, getAlerts);
+router.post('/', createAlert);
 
 export default router;
