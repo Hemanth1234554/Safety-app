57b32df86535b4fd7a4dfff6700b06ce805e4f6b
Hemanth1234554 <hemanthkummari5@gmail.com>
2025-12-11 22:47:04 +0530

    Added dashboard realted stuff

--- server/routes/alertRoutes.js
+++ server/routes/alertRoutes.js
@@ -1,14 +1,12 @@
 // File: server/routes/alertRoutes.js
 import express from 'express';
-import { createAlert, getAlerts } from '../controllers/alertController.js'; // Import getAlerts
+import { createAlert , getAlerts } from '../controllers/alertController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-// Route to SEND an alert (SOS)
-router.post('/', protect, createAlert);
+router.post('/', createAlert);
 
-// Route to GET history (The Vault)
 router.get('/', protect, getAlerts);
 
 export default router;
