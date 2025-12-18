7314f371d181193731d180ed28b0ac61866915bf
Hemanth1234554 <hemanthkummari5@gmail.com>
2025-12-18 20:50:01 +0530

    Rollback again

--- server/routes/alertRoutes.js
+++ server/routes/alertRoutes.js
@@ -1,14 +1,9 @@
 // File: server/routes/alertRoutes.js
 import express from 'express';
-import { createAlert, getAlerts } from '../controllers/alertController.js';
-import { protect } from '../middleware/authMiddleware.js'; // Import the Guard
+import { createAlert } from '../controllers/alertController.js';
 
 const router = express.Router();
 
-// POST /api/alerts -> Create a new SOS (Protected)
-router.post('/', protect, createAlert);
-
-// GET /api/alerts -> Get History (Protected)
-router.get('/', protect, getAlerts);
+router.post('/', createAlert);
 
 export default router;
